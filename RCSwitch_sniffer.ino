5c6909966ef253770c5d105b9d023254b35fb6e7
Vladimir Kozlov <vkozlov@tacitknowledge.com>
2019-05-02 14:36:23 +0300

    updated LCD

--- RCSwitch_sniffer.ino
+++ RCSwitch_sniffer.ino
@@ -1,26 +1,30 @@
-/*
-  Simple example for receiving
-  
-  https://github.com/sui77/rc-switch/
-*/
-
 #include <RCSwitch.h>
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <U8g2lib.h>
 
 RCSwitch mySwitch = RCSwitch();
-LiquidCrystal_I2C lcd(0x27,16,2);
+
+U8G2_PCD8544_84X48_1_4W_SW_SPI u8g2(U8G2_R0,/* clock=*/ 11, /* data=*/ 10, /* cs=*/ 7, /* dc=*/ 9, /* reset=*/ 8);  // Nokia 5110 Display
 
 void setup() {
   Serial.begin(9600);
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Ready");
+  u8g2.begin();
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_6x13_tr);
+    u8g2.setCursor(0, 10);
+    u8g2.print("433MHz sniffer");
+    
+    u8g2.setCursor(0, 25);
+    u8g2.print("...READY...");
+
+  } while ( u8g2.nextPage() );
+  
   mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
 }
 
 void loop() {
+  return;
   if (mySwitch.available()) {
     
     int value = mySwitch.getReceivedValue();
@@ -35,13 +39,26 @@ void loop() {
       Serial.print("bit ");
       Serial.print("Protocol: ");
       Serial.println( mySwitch.getReceivedProtocol() );
-      
-      lcd.setCursor(0,0);
-      lcd.print(mySwitch.getReceivedValue());
-      lcd.setCursor(1,1);
-      lcd.print( mySwitch.getReceivedBitlength() );
-      lcd.print("bit/p:");
-      lcd.println( mySwitch.getReceivedProtocol() );
+
+      u8g2.firstPage();
+      do {
+        u8g2.clear();
+        u8g2.setFont(u8g2_font_6x13_tr);
+        u8g2.setCursor(0, 10);
+        u8g2.print("433MHz sniffer");
+    
+        u8g2.setCursor(0, 25);
+        u8g2.print(mySwitch.getReceivedValue());
+
+        u8g2.setCursor(0, 35);
+        u8g2.print(mySwitch.getReceivedBitlength());
+        u8g2.print("bit");
+
+        u8g2.setCursor(0, 45);
+        u8g2.print("Protocol:");
+        u8g2.print(mySwitch.getReceivedProtocol());
+        
+      } while ( u8g2.nextPage() );
     }
 
     mySwitch.resetAvailable();
